b594f592b77d0dbf9522fe839846bb80e3ea8eaa
Bobby Ung <ung.bobby.huang@gmail.com>
2020-09-13 17:14:08 +0100

    create offers for the package

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="content">
-    <div>
-      <img v-bind:src="getImageUrl()" class="content__main-image" />
-    </div>
     <div class="content__pack-selection">
       <Button
         v-for="pack in trialPacks"
@@ -13,25 +10,43 @@
         v-on:click="selected = pack.size"
       ></Button>
     </div>
+    <img v-bind:src="getImageUrl()" class="content__main-image" />
+    <div class="content__pack-offers">
+      <h2 class="title">Your test package contains</h2>
+      <Offer
+        v-for="offer in getOffers()"
+        v-bind:key="offer.title"
+        :offer="offer"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import trialPacksJSON from "../data/trial.json";
+import offersJSON from "../data/offers.json";
 import Button from "./Button";
+import Offer from "./Offer";
 
 export default {
   name: "Content",
-  components: { Button },
+  components: { Button, Offer },
   methods: {
     getImageUrl() {
       return trialPacksJSON.find((pack) => pack.size === this.selected)
         .imageUrl;
     },
+    getOffers() {
+      const selectedPack = trialPacksJSON.find(
+        (pack) => pack.size === this.selected
+      );
+      return selectedPack.included.map((included) => offersJSON[included]);
+    },
   },
   data() {
     console.log(trialPacksJSON);
     return {
+      offers: offersJSON,
       trialPacks: trialPacksJSON,
       selected: "1",
     };
@@ -47,6 +62,8 @@ export default {
 
   &__main-image {
     width: 100%;
+    max-height: 600px;
+    max-width: 850px;
   }
 
   &__pack-selection {
---
+++ src/components/Offer.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="offer">
+    <div class="offer__image"><img v-bind:src="offer.imageUrl" /></div>
+    <div class="offer__description">
+      <div class="offer__description__title">{{ offer.title }}</div>
+      <div class="offer__description__reviews">
+        {{ offer.rating }} ({{ offer.reviewsNumber }})
+      </div>
+      <ul>
+        <li v-for="description in offer.descriptions" v-bind:key="description">
+          {{ description }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.offer {
+  display: flex;
+  margin: 10px 0;
+
+  &__image {
+    flex: 1;
+
+    img {
+      width: 100%;
+      min-width: 100px;
+    }
+  }
+
+  &__description {
+    flex: 2;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    margin-left: 10px;
+
+    &__title {
+      font-weight: bold;
+    }
+
+    ul {
+      padding-inline-start: 1.3em;
+    }
+
+    li {
+      text-align: left;
+    }
+  }
+}
+</style>
+<script>
+export default {
+  props: {
+    offer: Object,
+  },
+};
+</script>
