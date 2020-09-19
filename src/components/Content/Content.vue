e2846bedf7c626379a1ec38b77da400bcfd181ac
Bobby Ung <ung.bobby.huang@gmail.com>
2020-09-19 15:58:17 +0100

    add reviews and process static data

--- src/components/Content/Content.vue
+++ src/components/Content/Content.vue
@@ -8,19 +8,28 @@
       </div>
       <DeliveryInformations />
     </div>
-    <Offers :selectedPackage="selectedPackage" />
+    <div content="secondary-content">
+      <div class="content__process">
+        <h2>How it works</h2>
+        <img class="content__process--desktop" :src="processDesktop" />
+        <img class="content__process--mobile" :src="processMobile" />
+      </div>
+      <Offers :selectedPackage="selectedPackage" />
+      <Reviews />
+    </div>
   </div>
 </template>
 
 <script>
 import PackSelection from "./PackSelection";
 import DeliveryInformations from "./DeliveryInformations";
+import Reviews from "./Reviews";
 import Offers from "./Offers";
 import { mapState } from "vuex";
 
 export default {
   name: "Content",
-  components: { PackSelection, DeliveryInformations, Offers },
+  components: { PackSelection, DeliveryInformations, Offers, Reviews },
   methods: {
     getImageUrl() {
       const selectedPackage = this.packages.find(
@@ -33,6 +42,12 @@ export default {
     selectedPackage: (state) => state.selectedPackage,
     packages: (state) => state.packages,
   }),
+  data() {
+    return {
+      processDesktop: require("@/assets/process-desktop.svg"),
+      processMobile: require("@/assets/process-mobile.svg"),
+    };
+  },
 };
 </script>
 
@@ -41,6 +56,38 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  &__process {
+    text-align: center;
+    width: 100vw;
+    background-color: $background-grey-color;
+    margin-left: -30px;
+    max-width: 100%;
+    padding: 10px 30px;
+
+    @media (min-width: $desktop-viewport) {
+      margin-left: -100px;
+      max-width: initial;
+      padding: 0;
+    }
+
+    &--desktop {
+      padding: 30px 0;
+      display: none;
+      @media (min-width: $desktop-viewport) {
+        display: block;
+      }
+    }
+
+    &--mobile {
+      @media (min-width: $desktop-viewport) {
+        display: none;
+      }
+    }
+
+    h2 {
+    }
+  }
   &__main-image {
     width: 100%;
     max-height: 600px;
---
+++ src/components/Content/Reviews.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="reviews">
+    <h2 style="text-align: center">What others say about our diapers</h2>
+    <div
+      v-for="review in reviews"
+      v-bind:key="review.description"
+      class="review"
+    >
+      <div class="review__image">
+        <img :src="review.imageUrl" />
+      </div>
+      <div class="review__description">
+        <div class="review__description__details">
+          {{ review.description }}
+        </div>
+        <small class="review__person">
+          <span class="review__person__name"> {{ review.name }} </span> -
+          <span class="review__person__job">{{ review.job }} </span>
+        </small>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import reviews from "../../data/reviews.json";
+export default {
+  data() {
+    return { reviews };
+  },
+};
+</script>
+
+<style lang="scss">
+.reviews {
+  background-color: $background-grey-color;
+  width: 100vw;
+  margin-left: -30px;
+  max-width: 100%;
+  padding: 10px 30px;
+
+  @media (min-width: $desktop-viewport) {
+    margin-left: -100px;
+    padding: 20px 100px;
+  }
+}
+.review {
+  display: flex;
+  align-items: center;
+  margin: 20px 0;
+
+  &__image {
+    margin-right: 20px;
+    flex: 1;
+    img {
+      width: 100%;
+    }
+  }
+
+  &__description {
+    flex: 3;
+    display: flex;
+    flex-direction: column;
+  }
+
+  &__person {
+    &__name {
+      font-weight: bold;
+    }
+    &__job {
+      color: $primary-color;
+    }
+  }
+}
+</style>
